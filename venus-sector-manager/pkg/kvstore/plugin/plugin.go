5d76555de9577c1b2a89f4209aafa3c29c66b2c1
0x5459 <0x5459@protonmail.com>
2023-01-11 11:20:45 +0800

    WIP

--- venus-sector-manager/pkg/kvstore/plugin/plugin.go
+++ venus-sector-manager/pkg/kvstore/plugin/plugin.go
@@ -3,6 +3,7 @@ package kvstore
 import (
 	"github.com/ipfs-force-community/venus-cluster/venus-sector-manager/pkg/kvstore"
 	"github.com/ipfs-force-community/venus-cluster/venus-sector-manager/pkg/plugin"
+	pluginkvstore "github.com/ipfs-force-community/venus-cluster/venus-sector-manager/pkg/plugin/kvstore"
 )
 
 var plog = kvstore.Log.With("driver", "plugin")
@@ -14,7 +15,7 @@ func OpenPluginDB(path string, meta map[string]string) (kvstore.DB, error) {
 		// TODO(0x5459): error handing
 		return nil, err
 	}
-	db := plugin.DeclareKVStoreManifest(dbPlugin.Manifest)
+	db := pluginkvstore.DeclareKVStoreManifest(dbPlugin.Manifest)
 	plog.With("plugin_name", db.Name)
 	return db.Constructor(meta)
 }
